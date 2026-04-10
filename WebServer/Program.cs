350dda4770a40c289dfc097246eb935e2536454c
karasidnez <karasdnez@gmail.com>
2026-04-10 11:23:39 +0300

    Step 8: middleware logging and custom headers

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -3,6 +3,18 @@ using System.Data;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
+    await next(context);
+    Console.WriteLine($"[LOG] Ответ отправлен {context.Response.StatusCode}");
+});
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Powered-By", "ASP.NET Core Lab27");
+    await next(context);
+});
 
 app.MapGet("/", () => "Добро пожаловать на сервер!");
 
@@ -14,4 +26,28 @@ app.MapGet("/hello/{name}", (string name) => $"Привет, {name}!");
 
 app.MapGet("/sum/{a}/{b}", (int a, int b) => $"Возвращает сумму 2 чисел, {a + b}");
 
+app.MapGet("/student", () => new
+{
+    Name = "Иван Иванов",
+    Group = "ИСП - 231",
+    Year = 3,
+    IsActive = true
+});
+app.MapGet("/subjects", () => new[]
+{
+    "РПМ",
+    "РПМ",
+    "ИСРПО",
+    "СП",
+});
+
+app.MapGet("/product/{id}", (int id) => new Product(
+    Id: id,
+    Name: $"Товар #{id}",
+    Price: id * 99.99m,
+    InStock: id % 2 == 0
+));
+
 app.Run();
+
+record Product(int Id, string Name, decimal Price, bool InStock);
